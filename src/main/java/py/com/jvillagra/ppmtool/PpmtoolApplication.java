48d4b797172ad086e58a4471a5e047200b228538
Jazmin Villagra <jvillagra@sodep.com.py>
2019-01-18 15:11:25 -0300

    First commit

---
+++ src/main/java/py/com/jvillagra/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,14 @@
+package py.com.jvillagra.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolApplication.class, args);
+    }
+
+}
+
