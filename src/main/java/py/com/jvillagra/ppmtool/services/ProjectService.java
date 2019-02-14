10651098e9fafec58515042836359701a9c9838b
Jazmin Villagra <jvillagra@sodep.com.py>
2019-02-14 15:23:42 -0300

    Exception handlers created

--- src/main/java/py/com/jvillagra/ppmtool/services/ProjectService.java
+++ src/main/java/py/com/jvillagra/ppmtool/services/ProjectService.java
@@ -3,6 +3,7 @@ package py.com.jvillagra.ppmtool.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import py.com.jvillagra.ppmtool.domain.Project;
+import py.com.jvillagra.ppmtool.exceptions.ProjectIdException;
 import py.com.jvillagra.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -11,7 +12,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        //Logic
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectName().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase());
+        }
     }
 }
